50fdeb997284aaf0020e0a765d0266883d6a4d32
moddev <fnfzomb@gmail.com>
2026-08-07 00:03:36 +0300

    сделал некоторые изменине в main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::time;
 
-use bevy::{ecs::{query, system::entity_command::despawn}, prelude::*, state::commands};
+use bevy::{prelude::*};
+use rand::{RngExt, rng};
 
 fn main() {
     App::new()
@@ -8,14 +8,17 @@ fn main() {
         .insert_resource(Money{
             money: 0.0,
         })
+        .insert_resource(ZombieCount{
+            noraml: 0,
+            toxick: 0,
+            fat: 0,
+        },
+        )
         .add_systems(Startup, (
             camera_game, 
             fon_game, 
             command_center, 
             player_entety, 
-            setup_monster_zombie, 
-            setup_monster_toxick_zombie,
-            setup_monster_fat_zombie,
         ))
         .add_systems(Update, (
             economics,
@@ -26,12 +29,31 @@ fn main() {
             death_base,
             death_player,
             death_zombie,
-            hp_monster_logic,
+            // hp_monster_logic,
+            setup_monster_zombie,
+            setup_monster_toxick_zombie,
+            setup_monster_fat_zombie,
         ))
         .run();
 }
 
+
+// Компоненты зомби
+#[derive(Resource)]
+struct ZombieCount{
+    noraml: u32,
+    toxick: u32,
+    fat:  u32,
+}
+
+#[derive(Component)]
+enum ZombiType {
+    Normal,
+    Toxick,
+    Fat,
+}
 // Компоненты хп и домага
+
 
 #[derive(Component)]
 struct Hp {
@@ -123,6 +145,8 @@ fn player_entety(
 fn setup_monster_zombie(
     mut commands: Commands,
 ){
+    let mut rng = rng();
+    let y = rng.random_range(-80.0..80.0);
     commands.spawn((
         Monster{
             speed: 10.0,
@@ -138,14 +162,18 @@ fn setup_monster_zombie(
             Color::srgb(0.18, 0.19, 0.13),
             Vec2 ::new(20.0,80.0),
         ),
-        Transform::from_xyz(800.0, 20.0, 1.0)
+        Transform::from_xyz(800.0, y, 1.0),
     ));
+    println!("Зомби появился на координатах: y = {}", y);
 }
 
 //Проэктирование токсичного зомби
 fn setup_monster_toxick_zombie(
-    mut commands: Commands
+    mut commands: Commands,
 ){
+    
+    let mut rng = rng();
+    let y = rng.random_range(-80.0..80.0);
     commands.spawn((
         Monster{
             speed: 12.0,
@@ -161,14 +189,19 @@ fn setup_monster_toxick_zombie(
             Color::srgb(0.11, 0.08, 0.02),
             Vec2 ::new(20.0,60.0),
         ),
-
-        Transform::from_xyz(800.0,-10.0, 1.0)
+        Transform::from_xyz(800.0, y, 1.0),
     ));
+    println!("Токсичный зомби появился на координатах: y = {}", y);
 }
+
+
+
 //Проэктирование  толстого зомби
 fn setup_monster_fat_zombie(
     mut commands: Commands
 ){
+    let mut rng = rng();
+    let y = rng.random_range(-80.0..80.0);
     commands.spawn((
         Monster{
             speed: 3.0,
@@ -184,8 +217,9 @@ fn setup_monster_fat_zombie(
             Color::srgb(0.21, 0.38, 0.92),
             Vec2 ::new(20.0,60.0),
         ),
-        Transform::from_xyz(800.0, -60.0, 1.0)
+        Transform::from_xyz(800.0, y, 1.0),   
     ));
+    println!("Толстый зомби появился на координатах: y = {}", y);
 }
 
 // Логика передвижение
@@ -197,17 +231,21 @@ fn move_zombie(
         transform.translation.x -= monster.speed *  time.delta_secs();
     }
 }
-// Логика нанесение урона зомби
-fn hp_monster_logic(
-    player_query: Query<&Damage, With<Player>>,
-    mut monster_query: Query<&mut Hp, With<Monster>>,
-) {
-    let damage = player_query.single().unwrap().damage;
 
-    for mut hp in monster_query.iter_mut() {
-        println!("Зомби получил урон, осталось {}", hp.hp);
-    }
-}
+// Логика нанесение урона зомби
+// fn hp_monster_logic(
+//     player_query: Query<&Damage, With<Player>>,
+//     mut monster_query: Query<&mut Hp, With<Monster>>,
+// ) {
+//     let damage = player_query.single().unwrap().damage;
+
+//     for mut hp in monster_query.iter_mut() {
+//         hp.hp -= damage;
+//         println!("Зомби получил урон, осталось {}", hp.hp);
+//     }
+// }
+
+
 
 // Логика смерти
 fn death_base(
